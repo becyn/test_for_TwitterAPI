7118e81b6744776589cb754bd93af71542dfb16a
becyn <yucalcium.c6@gmail.com>
2011-11-23 23:00:12 -0800

    edited simple one.

--- code/sample_simpleAPI.rb
+++ code/sample_simpleAPI.rb
@@ -2,7 +2,7 @@
 #introduced at http://becyn.fluxflex.com/
 
 require 'twitter'
-    search = Twitter::Search.new
-    search.containing("becyn").language("ja").result_type("recent").each do |r|
-        p r
+search = Twitter::Search.new
+search.containing("becyn").language("ja").result_type("recent").each do |r|
+  p r
 end
