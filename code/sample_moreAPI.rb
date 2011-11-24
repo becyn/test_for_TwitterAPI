fdceca40bace7cfe287025dbe0727c6202109d36
becyn <yucalcium.c6@gmail.com>
2011-11-23 22:39:02 -0800

    rename sample_1.rb

--- code/sample_moreAPI.rb
+++ code/sample_moreAPI.rb
@@ -3,20 +3,20 @@ require 'twitter'
 search = Twitter::Search.new
 search.containing("becyn").language("ja").result_type("recent").each do |r|
     if /RT/ !=~ r
-#        p r.created_at
+##        p r.created_at
 #        p r.from_user_id
 #        p r.from_user_id_str
-#        p r.from_user_name
+        p r.from_user_name
 #        p r.geo
 #        p r.id
 #        p r.id_str
-#        p r.iso_language_code="ja"
-#        p r.result_type="recent"
-#        p r.profile_image_url="http://a2.twimg.com/profile_images/1598015957/billgates_normal.jpg" 
-#        p r.source
+#        p r.iso_language_code
+#?       p r.result_type
+##        p r.profile_image_url
+#        p r.source                  #yorufukurou
 #        p r.href
 #        p r.rel
-#        p r.text
+##        p r.text
 #        p r.to_user
 #        p r.to_user_id 
 #        p r.to_user_id_str
@@ -26,4 +26,5 @@ search.containing("becyn").language("ja").result_type("recent").each do |r|
 #    puts "#{r.from_user}: #{r.text}"
 end
 
+# last tweet from becyn
 p Twitter.user_timeline("becyn").first.text
