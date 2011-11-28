cf16f64b8564984e54631dd3e411bf0025019de6
becyn <yucalcium.c6@gmail.com>
2011-11-28 02:40:51 -0800

    edited code/sample_newAPI.rb to use TwitterAPI 2.0.0.

--- code/sample_newAPI.rb
+++ code/sample_newAPI.rb
@@ -1,31 +1,35 @@
+
 # -*- coding: utf-8 -*-
 # introduced at http://becyn.fluxflex.com/
 
 require "rubygems"
 require 'twitter'
 
-# search = Twitter.search("becyn -rt", :lang => "ja", :rpp => 5).each do |r|   # get 5 tweets except RT.
-search = Twitter.search("becyn", :lang => "ja").each do |r|   # get 15 tweets.
+search = Twitter.search("becyn -rt", :lang => "ja", :rpp => 1).each do |r|   # get 5 tweets except RT.
+# search = Twitter.search("becyn", :lang => "ja").each do |r|   # get 15 tweets.
 
         p r.created_at
-#        p r.from_user_id
+        p r.from_user_id
 #        p r.from_user_id_str
 #        p r.from_user_name
-#        p r.geo
-#        p r.id
+        p r.geo
+        p r.id
 #        p r.id_str
-#        p r.iso_language_code
-#?       p r.result_type
+        p r.iso_language_code
+#        p r.result_type
         p r.profile_image_url
-#        p r.source                  #yorufukurou
+        p r.source
 #        p r.href
 #        p r.rel
         p r.text
-#        p r.to_user
-#        p r.to_user_id
+        p r.to_user
+        p r.to_user_id
 #        p r.to_user_id_str
-#        p r.to_user_name
+#        puts r.to_user_name
 #        p r.location
+
+puts "------"
+        p r.methods
 
 # you can also get the tweets with onother syntax like following.
 #    puts "#{r.from_user}: #{r.text}"
